afa28eb7c5980c12e808aaacdfb92d254060d29a
abdulmajed123 <abdulmajed5618@gmail.com>
2026-03-29 20:11:49 +0600

    fix: update cors to use environment variable

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,7 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
